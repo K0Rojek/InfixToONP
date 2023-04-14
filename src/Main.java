cb3d8bdc95f33c064db529dc6965c7a7e033a014
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-14 13:29:53 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input =  "(15-3)^(3+2)*6/3=";
+        String output = infToONP.converseInfixToPostfix(input);
+        System.out.println(output);
     }
 }
 
