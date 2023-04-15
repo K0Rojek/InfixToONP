ef86e8a5a929ec75d409e4dbb9adadebb5fbb2f9
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-15 14:51:04 +0200

    university version is ready, code comments needs to be made

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 0; i < args.length; i++){
-            String input =  args[i];
+            String input = args[i];
             String inputInONP = infToONP.converseInfixToPostfix(input);
             double result = calculatorONP.equationSolver(inputInONP);
-            System.out.println("Input " + i + ": " + input);
+            System.out.println("Input " + i + ": \"" + input + "\"");
             System.out.println("Input in ONP " + i + ": " + inputInONP);
             System.out.println("Result " + i + ": " + result);
             System.out.println();
