4d139af4c813034a0f2b2eca37f43b82cf2d94f4
Kamil Rojek <kamilrojek2002@gmail.com>
2023-04-23 16:07:03 +0200

    postfix to infix conversion added

--- src/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ONP","l":"calculatorONP"},{"p":"ONP","l":"infixToONP"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
